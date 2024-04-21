4b209bcc8a52ba788a1b01603d1eeecf57b87730
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-04-21 10:44:31 +0200

    adding java.sql to the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module mszalewicz.trygghet {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.tomlj;
+    requires java.sql;
 
 
     opens mszalewicz.trygghet to javafx.fxml;
